ade21d91ad5454ba129345571d13b377bbee381e
Moustafa Shehata <167570674+MoustafaShehata@users.noreply.github.com>
2026-02-18 07:18:24 +0200

    Update MongoDB connection URI variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(ENV_VARS.MONGO_URI);
+    await mongoose.connect(ENV_VARS.MONGO_URL);
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting to database:" + error.message);
     process.exit(1);
   }
 };
+
