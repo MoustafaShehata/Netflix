5ac91f1d79db8bbbce6f36a74d3b4920c63e485d
Moustafa Shehata <167570674+MoustafaShehata@users.noreply.github.com>
2026-02-18 07:00:19 +0200

    Delete frontend directory

--- frontend/src/Pages/Home/HomePage.jsx
+++
@@ -1,7 +0,0 @@
-import AuthScreen from "./AuthScreen";
-import HomeScreen from "./HomeScreen";
-
-const HomePage = ({ user }) => {
-  return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
-};
-export default HomePage;
