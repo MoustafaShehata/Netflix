3adc441076ac7ffde88db62763bd60158886941f
Moustafa Shehata <167570674+MoustafaShehata@users.noreply.github.com>
2026-02-18 07:14:39 +0200

    Fix MONGO_URL reference in envVars.js

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,8 +6,9 @@ export const ENV_VARS = {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
-  MONGO_URL: process.env.MONGO_URI,
+  MONGO_URL: process.env.MONGO_URL,
   MAILTRAP_API_TOKEN: process.env.MAILTRAP_API_TOKEN,
   TMDB_API_TOKEN: process.env.TMDB_API_TOKEN,
   CLIENT_URL: process.env.CLIENT_URL,
 };
+
