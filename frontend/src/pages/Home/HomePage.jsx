40f2eea97f506047ea748bf2fe5cfb0910bb1f5d
Moustafa Shehata <167570674+MoustafaShehata@users.noreply.github.com>
2026-02-18 23:58:33 +0200

    Check user verification before rendering screens

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -2,6 +2,6 @@ import HomeScreen from "./HomeScreen";
 import AuthScreen from "./AuthScreen";
 
 const HomePage = ({ user }) => {
-  return <div>{user ? <HomeScreen /> : <AuthScreen />}</div>;
+  return <div>{user?.isVerified ? <HomeScreen /> : <AuthScreen />}</div>;
 };
 export default HomePage;
